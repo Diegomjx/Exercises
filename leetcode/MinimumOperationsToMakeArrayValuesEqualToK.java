2fd97e891996f75a73579f4f97caca3573be2faa
Diegomjx <19002145@galileo.edu>
2025-04-09 12:57:54 -0600

    v0.0.3   Faster version 100% in leetcode but use more memory

--- leetcode/MinimumOperationsToMakeArrayValuesEqualToK.java
+++ leetcode/MinimumOperationsToMakeArrayValuesEqualToK.java
@@ -2,17 +2,20 @@
 // [4,2,4,4,4]   4->2
 // [2,2,2,2,2]   h=2
 
-import java.util.HashSet;
 
 class Solution {
     public int minOperations(int[] nums, int k) {
-        HashSet<Integer> uniqueValues = new HashSet<>();
-        
+        boolean[] seen = new boolean[101]; 
+        int uniqueCount = 0;
+
         for (int num : nums) {
             if (num < k) return -1;  
-            if (num != k) uniqueValues.add(num);
+            if (num != k && !seen[num]) {
+                seen[num] = true;
+                uniqueCount++;
+            }
         }
-        
-        return uniqueValues.size();
+
+        return uniqueCount;
     }
 }
