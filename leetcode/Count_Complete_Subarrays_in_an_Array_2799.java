112ca13437c0bbff55cf6c9ae8b63cae21c734d0
Diegomjx <19002145@galileo.edu>
2025-04-24 13:21:13 -0600

    Runtime optimization

--- leetcode/Count_Complete_Subarrays_in_an_Array_2799.java
+++ leetcode/Count_Complete_Subarrays_in_an_Array_2799.java
@@ -3,31 +3,21 @@
 import java.util.*;
 
 class Solution {
-    public int countCompleteSubarrays(int[] nums) {
-        int n = nums.length;
-
-        // Step 1: Get total distinct elements
-        Set<Integer> uniqueSet = new HashSet<>();
-        for (int num : nums) uniqueSet.add(num);
-        int totalDistinct = uniqueSet.size();
-
-        int left = 0, result = 0;
-        Map<Integer, Integer> freqMap = new HashMap<>();
-
-        for (int right = 0; right < n; right++) {
-            freqMap.put(nums[right], freqMap.getOrDefault(nums[right], 0) + 1);
-
-            while (freqMap.size() == totalDistinct) {
-                result += n - right;
-                int leftVal = nums[left];
-                freqMap.put(leftVal, freqMap.get(leftVal) - 1);
-                if (freqMap.get(leftVal) == 0) {
-                    freqMap.remove(leftVal);
-                }
-                left++;
+    public int countCompleteSubarrays(int[] a) {
+        int n = a.length, r = 0, l = 0, out = 0;
+        Set<Integer> d = new HashSet<>();
+        for (int x : a) d.add(x);
+        int D = d.size();
+        Map<Integer, Integer> m = new HashMap<>();
+        for (; r < n; r++) {
+            m.put(a[r], m.getOrDefault(a[r], 0) + 1);
+            while (m.size() == D) {
+                out += n - r;
+                m.put(a[l], m.get(a[l]) - 1);
+                if (m.get(a[l]) == 0) m.remove(a[l]);
+                l++;
             }
         }
-
-        return result;
+        return out;
     }
 }
